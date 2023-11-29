f7c2db470b91e0a27015b0fd49e441ca84005b09
shaikareef081 <shaikareef482@gmail.com>
2023-11-29 18:52:57 +0530

    purshed order of the user

---
+++ frontend/src/redux/actions/order.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { server } from "../../server";
+
+export const getAllOrdersOfUser = (userId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllOrdersUserRequest",
+    });
+
+    const { data } = await axios.get(`${server}/order/get-all-order/${userId}`);
+
+    dispatch({
+      type: "getAllOrdersUserSuccess",
+      payload: data.orders,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllOrdersUserFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getAllOrdersOfShop = (shopId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllOrdersShopRequest",
+    });
+
+    const { data } = await axios.get(
+      `${server}/order/get-seller-all-orders/${shopId}`
+    );
+
+    dispatch({
+      type: "getAllOrdersShopSuccess",
+      payload: data.orders,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllOrdersShopFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
