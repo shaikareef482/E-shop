c1e60ab4be4d6ecb333c38c1c1fe9f71c833e045
shaikareef081 <shaikareef482@gmail.com>
2023-11-29 18:55:21 +0530

    seller dashboard

---
+++ frontend/src/routes/SellerProtectedRouter.js
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Loader from "../components/Layout/Loader";
+
+import React from 'react';
+
+const SellerProtectedRouter = ({children}) => {
+    const {isLoading,isSeller} = useSelector((state)=>state.seller)
+    if(isLoading === true)
+    {
+        return <Loader/>;
+    }else{
+        if(!isSeller)
+        {
+            return <Navigate to={`/shop-login`} replace/>
+        }
+        return children;
+    }
+}
+
+export default SellerProtectedRouter;
