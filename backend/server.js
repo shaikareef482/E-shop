6fd09808b3c94c166efcedf218f142bf004d579f
shaikareef081 <shaikareef482@gmail.com>
2023-12-05 22:21:45 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET","POST","PUT","PATCH","DELETE"],
   })
 );
